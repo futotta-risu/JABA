b988a7ec194ef1237c1e3ec07e2830f9bc81e2aa
Restyled.io <commits@restyled.io>
2021-06-15 22:49:03 +0000

    Restyled by black

--- tests/JATS/test_analyzer.py
+++ tests/JATS/test_analyzer.py
@@ -7,19 +7,23 @@ a = Analyzer()
 
 
 def test_get_sentiment_positive():
-    assert a.get_sentiment(
-        "I am very happy because Bitcoin is growing up") >= 0.5
+    assert a.get_sentiment("I am very happy because Bitcoin is growing up") >= 0.5
 
 
 def test_get_sentiment_negative():
-    assert a.get_sentiment(
-        "I am very afraid of the Bitcoin price changes, i think i am going to lose all my money by taking this risk") <= -0.5
+    assert (
+        a.get_sentiment(
+            "I am very afraid of the Bitcoin price changes, i think i am going to lose all my money by taking this risk"
+        )
+        <= -0.5
+    )
 
 
 def test_get_sentiment_neutral():
     sentiment = a.get_sentiment("Hi, my name is Lander")
     assert sentiment <= 0.5
     assert sentiment >= -0.5
+
 
 # TODO: Crear este test
 
@@ -29,14 +33,16 @@ def test_analyze():
 
 
 def test_get_cosine_similarity():
-    cleaned_texts = ['BILBAO', 'BILBAO', 'MADRID', 'MADRD', 'VALENCIA']
-    expected_array = np.array([
-        [1, 1, 0, 0, 0],
-        [1, 1, 0, 0, 0],
-        [0, 0, 1, 0, 0],
-        [0, 0, 0, 1, 0],
-        [0, 0, 0, 0, 1]
-    ])
+    cleaned_texts = ["BILBAO", "BILBAO", "MADRID", "MADRD", "VALENCIA"]
+    expected_array = np.array(
+        [
+            [1, 1, 0, 0, 0],
+            [1, 1, 0, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 1],
+        ]
+    )
     csim = a.get_cosine_similarity(cleaned_texts)
     comparison = csim == expected_array
     assert comparison.all() == True
