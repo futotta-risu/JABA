caca7d8535113ddaa5b767d7b725d4d5dacb1e10
Restyled.io <commits@restyled.io>
2021-06-17 09:58:09 +0000

    Restyled by black

--- JABA/utils/BTCScrapper.py
+++ JABA/utils/BTCScrapper.py
@@ -28,8 +28,7 @@ def minutes_of_new_data(symbol, kline_size, data, source):
         old = datetime.strptime("1 Jan 2017", "%d %b %Y")
     if source == "binance":
         new = pd.to_datetime(
-            binance_client.get_klines(symbol=symbol,
-                                      interval=kline_size)[-1][0],
+            binance_client.get_klines(symbol=symbol, interval=kline_size)[-1][0],
             unit="ms",
         )
     return old, new
@@ -41,19 +40,21 @@ def get_all_binance(symbol, kline_size, save=False):
         data_df = pd.read_csv(filename)
     else:
         data_df = pd.DataFrame()
-    oldest_point, newest_point = minutes_of_new_data(symbol,
-                                                     kline_size,
-                                                     data_df,
-                                                     source="binance")
+    oldest_point, newest_point = minutes_of_new_data(
+        symbol, kline_size, data_df, source="binance"
+    )
     delta_min = (newest_point - oldest_point).total_seconds() / 60
     available_data = math.ceil(delta_min / binsizes[kline_size])
     if oldest_point == datetime.strptime("1 Jan 2017", "%d %b %Y"):
-        print("Downloading all available %s data for %s. Be patient..!" %
-              (kline_size, symbol))
+        print(
+            "Downloading all available %s data for %s. Be patient..!"
+            % (kline_size, symbol)
+        )
     else:
         print(
             "Downloading %d minutes of new data available for %s, i.e. %d instances of %s data."
-            % (delta_min, symbol, available_data, kline_size))
+            % (delta_min, symbol, available_data, kline_size)
+        )
     klines = binance_client.get_historical_klines(
         symbol,
         kline_size,
