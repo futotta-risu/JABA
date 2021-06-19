31a67aea785965052d63bbbab87a1692c7b71a9a
Lander San Millan <landersanmillan@opendeusto.es>
2021-06-19 15:28:08 +0200

    Redefined

--- tests/JATS/test_cleaner.py
+++ tests/JATS/test_cleaner.py
@@ -4,4 +4,4 @@ from JABA.service.scrapper import cleaner
 
 def test_clean_tweet_lowers_case():
 	
-	assert clean_tweet("Hi, im Erik") == "hi, im erik","Problem while cleaning tweet"
+	assert cleaner.clean_tweet("Hi, im Erik") == "hi, im erik","Problem while cleaning tweet"
