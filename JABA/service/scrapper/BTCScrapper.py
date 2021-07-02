64c61a5b72629921501ef5f4d8a7e84a92faea62
Lander San Millan <landersanmillan@opendeusto.es>
2021-07-02 14:00:40 +0200

    Cleaned directory & retrieved BTCScrapper

--- JABA/service/scrapper/BTCScrapper.py
+++ JABA/service/scrapper/BTCScrapper.py
@@ -13,7 +13,7 @@ binance_api_key = '[REDACTED]'    #Enter your own API-key here
 binance_api_secret = '[REDACTED]' #Enter your own API-secret here
 
 ### CONSTANTS
-binsizes = {"1m": 1, "5m": 5, "1h": 60, "1d": 1440}
+binsizes = {"1m": 1, "5m": 5, "30m":30, "1h": 60, "1d": 1440}
 batch_size = 750
 binance_client = Client(api_key=binance_api_key, api_secret=binance_api_secret)
 
@@ -21,7 +21,7 @@ binance_client = Client(api_key=binance_api_key, api_secret=binance_api_secret)
 ### FUNCTIONS
 def minutes_of_new_data(symbol, kline_size, data, source):
     if len(data) > 0:  old = parser.parse(data["timestamp"].iloc[-1])
-    elif source == "binance": old = datetime.strptime('1 Jan 2017', '%d %b %Y')
+    elif source == "binance": old = datetime.strptime('1 Jan 2014', '%d %b %Y')
     if source == "binance": new = pd.to_datetime(binance_client.get_klines(symbol=symbol, interval=kline_size)[-1][0], unit='ms')
     return old, new
 
@@ -32,7 +32,7 @@ def get_all_binance(symbol, kline_size, save = False):
     oldest_point, newest_point = minutes_of_new_data(symbol, kline_size, data_df, source = "binance")
     delta_min = (newest_point - oldest_point).total_seconds()/60
     available_data = math.ceil(delta_min/binsizes[kline_size])
-    if oldest_point == datetime.strptime('1 Jan 2017', '%d %b %Y'): print('Downloading all available %s data for %s. Be patient..!' % (kline_size, symbol))
+    if oldest_point == datetime.strptime('1 Jan 2014', '%d %b %Y'): print('Downloading all available %s data for %s. Be patient..!' % (kline_size, symbol))
     else: print('Downloading %d minutes of new data available for %s, i.e. %d instances of %s data.' % (delta_min, symbol, available_data, kline_size))
     klines = binance_client.get_historical_klines(symbol, kline_size, oldest_point.strftime("%d %b %Y %H:%M:%S"), newest_point.strftime("%d %b %Y %H:%M:%S"))
     data = pd.DataFrame(klines, columns = ['timestamp', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_av', 'trades', 'tb_base_av', 'tb_quote_av', 'ignore' ])
