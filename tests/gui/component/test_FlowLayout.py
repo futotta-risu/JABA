13c3de38bd172c0a3c21cfc7dcb575dbc06414bc
Erik Terres <37312201+erikberter@users.noreply.github.com>
2021-07-11 23:28:46 +0200

    Added qtbot to FlowLayout

--- tests/gui/component/test_FlowLayout.py
+++ tests/gui/component/test_FlowLayout.py
@@ -9,18 +9,18 @@ from PyQt5.QtWidgets import QApplication
 app = QApplication(sys.argv)
 
 
-def test_create_flowlayout():
+def test_create_flowlayout(qtbot):
     layout = FlowLayout()
     
     assert layout != None
 
 
-def test_has_height_for_width():
+def test_has_height_for_width(qtbot):
     layout = FlowLayout()
     
     assert layout.hasHeightForWidth()
     
-def test_count():
+def test_count(qtbot):
     layout = FlowLayout()
     
     assert layout.count() == 0
