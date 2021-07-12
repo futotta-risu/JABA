bbab9c19755e3d5e1e5be828aefa04e02bbed8ec
Erik Terres <37312201+erikberter@users.noreply.github.com>
2021-07-12 15:53:40 +0200

    Test model generics and cleaner

--- tests/model/test_FileManager.py
+++ tests/model/test_FileManager.py
@@ -12,3 +12,33 @@ def test_create_file_manager():
         # Then
     except Exception:
         pytest.fail("Could not create FileManagerInterface")
+
+def test_file_manager_get_name_not_invalid():
+    # Given
+    file_manager = FileManagerInterface()
+    # When
+    try:
+        file_manager.get_file_name({})
+        # Then
+    except Exception:
+        pytest.fail("Could not create get name")
+        
+def test_file_manager_open_file_not_invalid():
+    # Given
+    file_manager = FileManagerInterface()
+    # When
+    try:
+        file_manager.open_file({})
+        # Then
+    except Exception:
+        pytest.fail("Could not create open file")
+        
+def test_file_manager_save_file_not_invalid():
+    # Given
+    file_manager = FileManagerInterface()
+    # When
+    try:
+        file_manager.save_file([], {})
+        # Then
+    except Exception:
+        pytest.fail("Could not save file")
