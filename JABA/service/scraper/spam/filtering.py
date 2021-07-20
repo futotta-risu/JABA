d5903146b234429f74e93cba7e98522957f784ad
Erik Terres <37312201+erikberter@users.noreply.github.com>
2021-07-20 18:08:21 +0200

    Concurrent spam filtering

--- JABA/service/scraper/spam/filtering.py
+++ JABA/service/scraper/spam/filtering.py
@@ -1,36 +1,87 @@
 from math import ceil
+from multiprocessing import Pool
+import time
 
 from sklearn.cluster import DBSCAN
 from scipy.spatial.distance import pdist, squareform
 
 import numpy as np
+import pandas as pd
 
 from .metrics import jacard
 
+def chunks(lst, n):
+    """
+        Yield successive n-sized chunks from lst.
 
-def filter_duplicated(data):
+        https://stackoverflow.com/questions/312443
+    """
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+
+def filter_duplicated(data : list):
     '''
         Filters the duplicated elements from the data.
 
         Parameters:
             data (list) List of strings.
     '''
-    pass  # TODO Implement functions
+    return list(dict.fromkeys(data))
+
+def filter_duplicated_pandas(
+    data : pd.DataFrame,
+    column : str = "Text"
+    ):
+    '''
+        Filters the duplicated elements from the data and return cleaned data and duplicates.
+
+        Parameters:
+            data (pd.DataFrame) DataFrame
+    '''
+    if column not in data.columns:
+        raise ValueError("Column must be in dataframe to use it in the filtering.")
+
+    data = data[(data["Text"].notna()) & data["Text"]] # Remove Nan's
+
+    duplicated_counts = data[data["Text"].duplicated()]["Text"].value_counts()
+    data_spam = duplicated_counts.rename_axis("unique_texts").reset_index(name="counts")
+
+    data.drop_duplicates(subset="Text", keep='first', inplace=True)
+
+    return data, data_spam
+
+def filter_spam_batch(batch : list, metric = jacard, eps : float =0.3):
+    ''' Filters spam batch based on text similarity '''
+    batch = np.array(batch).reshape(-1, 1)
+    distance_matrix = squareform(pdist(batch, metric))
+    db = DBSCAN().fit(distance_matrix)
+
+    return [batch[i] for i, cat in enumerate(db.labels_) if cat == -1]
 
 
-def filter_spam(data, batch_size=5000, verbose=False, metric=jacard, eps=0.3):
+# TODO Implement version for pandas
+def filter_spam(
+    data : list, 
+    batch_size : int = 5000,
+    verbose : bool = False,
+    metric = jacard,
+    eps : float = 0.3):
     '''
         Filters spam based on text similarity.
 
         Parameters
             data : Python list of the input data. The list can be a string.
     '''
+    if batch_size < 1:
+        raise ValueError("Batch size must be a positive integer")
+
     batches = ceil(len(data)/batch_size)
 
     filtered_data = []
 
     if verbose:
-        import time
+        
         start_time = time.time()
 
     for n_batch in range(batches):
@@ -46,12 +97,24 @@ def filter_spam(data, batch_size=5000, verbose=False, metric=jacard, eps=0.3):
 
         batch = data[batch_size * n_batch:batch_size * (n_batch+1)]
 
-        batch = np.array(batch).reshape(-1, 1)
-        distance_matrix = squareform(pdist(batch, metric))
-        db = DBSCAN().fit(distance_matrix)
-
-        for i, v in enumerate(db.labels_):
-            if v == -1:
-                filtered_data += [batch[i]]
+        filtered_data += filter_spam_batch(batch, metric = metric, eps = eps)
+        
 
     return filtered_data
+
+
+
+def filter_spam_concurrent(data,  batch_size : int = 5000, verbose=False, metric=jacard, eps=0.3):
+    ''' Filters spam from data based on text similarity concurrently '''
+    if batch_size < 1:
+        raise ValueError("Batch size must be a positive integer")
+
+    batches = chunks(data, batch_size)
+    del data
+
+    result_data = []
+
+    with Pool(5) as pool:
+        result_data = pool.map(filter_spam_batch, batches)
+
+    return result_data
--- tests/service/scraper/spam/test_filtering.py
+++ tests/service/scraper/spam/test_filtering.py
@@ -1,20 +1,86 @@
 import pytest
 
+import pandas as pd
+
+from service.scraper.spam.filtering import filter_duplicated
+from service.scraper.spam.filtering import filter_duplicated_pandas
+
 from service.scraper.spam.filtering import filter_spam
+from service.scraper.spam.filtering import filter_spam_concurrent
 
 from unittest.mock import Mock
 
-import pandas as pd
+simple_example = [
+    'Hi loved', 'Hi loved', 'Hi loved', 'Hi loved',
+    'Hi loved', 'Hi loved', 'Hi loved', 'True Fact',
+    'Non Fact'
+]
+
+def test_filter_duplicated():
+    # Given
+    data = simple_example
+
+    # When
+    result = filter_duplicated(data)
+
+    # Then
+    assert result.sort() == ['Hi loved', 'True Fact', 'Non Fact'].sort()
+
+def test_filter_duplicated_pandas():
+    # Given
+    data = simple_example
+    data_pandas = pd.DataFrame({'Text': data})
+    # When
+    result, spam = filter_duplicated_pandas(data_pandas)
+
+    # Then
+    assert result['Text'].to_list().sort() == ['Hi loved', 'True Fact', 'Non Fact'].sort()
+    assert spam.iloc[0]['unique_texts'] == 'Hi loved'
+
+def test_filter_duplicated_pandas_with_invalid_column():
+    # Given
+    data = simple_example
+    data_pandas = pd.DataFrame({'Text': data})
+    # When
+    with pytest.raises(ValueError):
+        filter_duplicated_pandas(data_pandas, column = 'Random')
+
+def test_pandas_and_default_duplicate_are_same():
+    # Given
+    data = simple_example
+    data_pandas = pd.DataFrame({'Text': data})
+    # When
+    result = filter_duplicated(data)
+    result_pandas, _ = filter_duplicated_pandas(data_pandas)
+
+    # Then
+    assert result.sort() == result_pandas['Text'].to_list().sort()
 
 def test_filter_spam():
     # Given
-    data = [
-        'Hi loved', 'Hi loved', 'Hi loved', 'Hi loved',
-        'Hi loved', 'Hi loved', 'Hi loved', 'True Fact'
-        ]
+    data = simple_example
 
     # When
     result = filter_spam(data, verbose=True)
 
     # Then
-    assert len(result) == 1
+    assert result == ['True Fact', 'Non Fact']
+
+@pytest.mark.parametrize("batch_size", [-1, 0])
+def test_filter_spam_fails_with_invalid_batch_size(batch_size):
+    # Given
+    data = simple_example
+
+    # When
+    with pytest.raises(ValueError):
+        filter_spam(data, batch_size = batch_size)
+
+def test_filter_spam_concurrent():
+    # Given
+    data = simple_example
+
+    # When
+    result = filter_spam_concurrent(data, batch_size=5)
+
+    # Then
+    assert result.sort() == ['True Fact', 'Non Fact'].sort()
